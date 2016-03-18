da6f61461b6f62acbd508aef9f482dd790aa38fc
Peter Clarke <PC1996@msn.com>
2016-03-18 14:43:42 -0400

    Implemented bubble sort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -1,14 +1,29 @@
 package Sorting;
 public class BubbleSort {
-	/**
-	 * bubbleSort				sorts an array of integers using bubble sort
-	 * 
-	 * @param unsortedArray		an unsorted array of integers
-	 * @return					the input array, sorted least to greatest
-	 */
-	public static int[] bubbleSort(int[] unsortedArray) {
-		//TODO: S1 implements bubble sort
-		
-		return null;
-	}
+    /**
+     * bubbleSort sorts an array of integers using bubble sort
+     * 
+     * @param unsortedArray	an unsorted array of integers
+     * @return the input array, sorted least to greatest
+     */
+    public static int[] bubbleSort(int[] unsortedArray) {
+        int len = array.length;
+        boolean swapped = true;
+
+        while (swapped) {
+            swapped = false;
+            for (int i = 1; i < len; i++) {
+                // if this pair is out of order
+                if (array[i-1] > array[i]) {
+                    // swap them
+                    int tmp = array[i-1];
+                    array[i-1] = array[i];
+                    array[i] = tmp;
+                    swapped = true;
+                }
+            }
+            len -= 1; // optimizes the for loop iteration
+        }
+        return array;
+    }
 }
--- src/Sorting/Main.java
+++ src/Sorting/Main.java
@@ -2,28 +2,30 @@ package Sorting;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		//Generates a random array
-		int[] unsortedArray = new int[(int) (Math.random()*20.0)];
-		for (int i = 0; i < unsortedArray.length; i++) {
-			unsortedArray[i] = (int) (Math.random()*20.0);
-		}
-		
-		System.out.println("Unsorted Array:");
-		
-		for (int i: unsortedArray) {
-				System.out.print(i + " ");
-		}
-		System.out.println();
-		
-		System.out.println("Sorted Array:");
-		//TODO: sort the array and print out its contents
-		
-		
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        //Generates a random array
+        int[] unsortedArray = new int[(int) (Math.random()*20.0)];
+        for (int i = 0; i < unsortedArray.length; i++) {
+            unsortedArray[i] = (int) (Math.random()*20.0);
+        }
+
+        System.out.println("Unsorted Array:");
+
+        for (int i: unsortedArray) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.println("Sorted Array:");
+        int[] sorted = BubbleSort.bubbleSort(unsortedArray);
+        for (int i : sortedArray) {
+            System.out.print(i + " ");
+        }
+
+    }
 
 }
