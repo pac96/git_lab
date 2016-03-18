dd2ab7cf8db929334cb05113b7da2d305540a356
Jordan White <jordan_a_white@brown.edu>
2016-03-18 14:43:46 -0400

    Added InsertionSort implementation

--- src/Sorting/Main.java
+++ src/Sorting/Main.java
@@ -2,30 +2,31 @@ package Sorting;
 
 public class Main {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-
-        //Generates a random array
-        int[] unsortedArray = new int[(int) (Math.random()*20.0)];
-        for (int i = 0; i < unsortedArray.length; i++) {
-            unsortedArray[i] = (int) (Math.random()*20.0);
-        }
-
-        System.out.println("Unsorted Array:");
-
-        for (int i: unsortedArray) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        System.out.println("Sorted Array:");
-        int[] sorted = BubbleSort.bubbleSort(unsortedArray);
-        for (int i : sortedArray) {
-            System.out.print(i + " ");
-        }
-
-    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//Generates a random array
+		int[] unsortedArray = new int[(int) (Math.random()*20.0)];
+		for (int i = 0; i < unsortedArray.length; i++) {
+			unsortedArray[i] = (int) (Math.random()*20.0);
+		}
+		
+		System.out.println("Unsorted Array:");
+		
+		for (int i: unsortedArray) {
+				System.out.print(i + " ");
+		}
+		System.out.println();
+		
+		System.out.println("Sorted Array:");
+		//TODO: sort the array and print out its contents
+		int[] sortedArray = InsertionSort.insertionSort(unsortedArray);
+		for (int i : sortedArray)
+		{
+		    System.out.println(i + " ");
+		}
+	}
 
 }
